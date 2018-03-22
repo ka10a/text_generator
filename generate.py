d2a8ac56e99771cb4443eef35a2da128f6f61112
Shaydurova Ekaterina <ka10a@mail.ru>
2018-03-23 00:21:20 +0300

    Added code

---
+++ generate.py
@@ -0,0 +1,39 @@
+import numpy
+import argparse
+import pickle
+
+parser = argparse.ArgumentParser(description="Hello another one time.", epilog="")
+parser.add_argument('--model', required=True, type=str, nargs=1, default='statistics.out', help='')
+parser.add_argument('--seed', type=str, nargs=1, default='', help='')
+parser.add_argument('--length', required=True, type=int, nargs=1, default=7, help='')
+parser.add_argument('--output', type=str, nargs=1, default='stdout', help='0')
+args = parser.parse_args()
+
+FILE_IN = open(args.model[0], mode='rb')
+STATISTICS = pickle.load(FILE_IN)
+if type(dict()) != type(STATISTICS):
+    print('Wrong file!!!')
+    exit(0)
+
+"""
+for line in FILE_IN.readlines():
+    word1, word2, frequency2, frequency1 = line.split()
+    frequency = int(frequency2) / int(frequency1)
+    if word1 in STATISTICS:
+        STATISTICS[word1][0].append(word2)
+        STATISTICS[word1][1].append(frequency)
+    else:
+        STATISTICS[word1] = ([word2], [frequency])
+"""
+
+word1 = '*BEGIN*'
+CNT = args.length[0]
+while CNT > 1:
+    word2 = numpy.random.choice(list(STATISTICS[word1].keys()), 1, list(STATISTICS[word1].values()))[0]
+    if word2 == '*BEGIN*' or word2 == '*END*':
+        word1 = word2
+        continue
+    print(word2, end=' ')
+    CNT -= 1
+    word1 = word2
+print(*numpy.random.choice(list(STATISTICS[word1].keys()), 1, list(STATISTICS[word1].values())))
---
+++ train.py
@@ -0,0 +1,86 @@
+import re
+import argparse
+import os
+import pickle
+
+
+def make_stat(statistic, parts, statistics):
+    for i in range(len(parts) - 1):
+        if len(parts[i]) == 0:
+            continue
+        word = parts[i]
+        if word not in statistics:
+            statistics[word] = 0
+        statistics[word] += 1
+        next_word = parts[i + 1]
+        if word in statistic:
+            if next_word not in statistic[word]:
+                statistic[word][next_word] = 0
+            statistic[word][next_word] += 1
+        else:
+            statistic[word] = {next_word: 1}
+
+
+def parse_sentence(line, all_collocations, frequencies):
+    sentences = line.split('. ')
+    for sentence in sentences:
+        parse_words = re.sub(r"[^\w]", ' ', sentence)
+        if args.lc:
+            parse_words = parse_words.lower()
+        words = [BEGIN] + parse_words.split() + [END]
+        if len(words) < 3:
+            continue
+        make_stat(all_collocations, words, frequencies)
+
+
+parser = argparse.ArgumentParser(description="Hi, Wokinshopash!", epilog="You're nice! Goodbye.")
+parser.add_argument('--input-dir', type=str, nargs=1, default='stdin', help='')
+parser.add_argument('--model', required=True, type=str, nargs=1, default='statistics.out', help='')
+parser.add_argument('--lc', nargs=1)
+args = parser.parse_args()
+
+DIR = args.input_dir[0]
+if DIR != 'stdin':
+    try:
+        FILES = os.listdir(DIR)
+    except OSError:
+        print("Directory doesn't exist.")
+        exit(1)
+
+    TXT_FILES = filter(lambda x: x.endswith('.txt'), FILES)
+
+
+BEGIN = '*BEGIN*'
+END = '*END*'
+ALL_COLLOCATIONS = {END: {BEGIN: 1}}
+FREQUENCIES = {END: 1}
+
+if DIR != 'stdin':
+    flag = True
+    for filename in TXT_FILES:
+        flag = False
+        fin = open(filename, "r")
+        for new_line in fin.readlines():
+            parse_sentence(new_line, ALL_COLLOCATIONS, FREQUENCIES)
+    if flag:
+        print("There're no txt-files in directory.")
+        exit(1)
+else:
+    string = input()
+    parse_sentence(string)
+
+for word1, dict1 in ALL_COLLOCATIONS.items():
+    frequency = FREQUENCIES[word1]
+    for word2 in dict1.keys():
+        dict1[word2] /= frequency
+
+FOUT = open(args.model[0], mode='wb')
+pickle.dump(ALL_COLLOCATIONS, FOUT)
+FOUT.close()
+
+"""
+collocations = dict()
+for word1, pairs in all_collocations.items():
+    for word2, stat in pairs.items():
+        print(word1, word2, stat, frequencies[word1], file=FILE_OUT)
+"""
