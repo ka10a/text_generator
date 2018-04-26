2fdef716bacc950de56423a2633977b875f05f2a
ka10a <ka10a@mail.ru>
2018-04-26 13:41:52 +0300

    Вынесено тело программ в main() Весь файл не загружается в память Refactoring

--- generate.py
+++ generate.py
@@ -5,47 +5,56 @@ import pickle
 
 def is_service_word(word):
     # It proves that word is BEGIN = None or END = None
-    if word is None:
-        return True
-    return False
+    return word is None
 
 
-parser = argparse.ArgumentParser(description="Hello another one time.", epilog="")
-parser.add_argument('--model', required=True, type=str, nargs=1,
-                    help="Way with file's name to directory, where file with model is.")
-parser.add_argument('--length', type=int, help='Length of text what you want.')
-parser.add_argument('--seed', type=str, nargs=1, default=[None], help="First word")
-args = parser.parse_args()
+def read_stat(arguments):
+    # Generate text word by word
+    # Get dict of collocations and statistic
+    file_in = open(arguments.model[0], mode='rb')
+    stat = pickle.load(file_in)
 
-# Get dict of collocations and statistic
-FILE_IN = open(args.model[0], mode='rb')
-STATISTICS = pickle.load(FILE_IN)
-if not isinstance(STATISTICS, dict):
-    print(type(STATISTICS))
-    print('Wrong file!!!')
-    exit(1)
+    if not isinstance(stat, dict):
+        print("Wrong file!!! Your file's type isn't dict.")
+        exit(1)
 
-# Generate text word by word
-word1 = args.seed[0]
-if word1 not in STATISTICS.keys():
-    print("There is no word '{}'.".format(word1))
-    exit(1)
+    return stat
 
-CNT = args.length
-if word1 is not None:
-    print(word1, end='')
-for _ in range(CNT, 1, -1):
-    word2 = numpy.random.choice(list(STATISTICS[word1].keys()), 1, list(STATISTICS[word1].values()))[0]
-    if is_service_word(word2):
-        print('.', end='')
+
+def main():
+    parser = argparse.ArgumentParser(description="Hello another one time.", epilog="")
+    parser.add_argument('--model', required=True, type=str, nargs=1,
+                        help="Way with file's name to directory, where file with model is.")
+    parser.add_argument('--length', type=int, help='Length of text what you want.')
+    parser.add_argument('--seed', type=str, nargs=1, default=[None], help="First word")
+    args = parser.parse_args()
+
+
+    statistics = read_stat(args)
+    word1 = args.seed[0]
+    if word1 not in statistics.keys():
+        print("There is no word '{}'.".format(word1))
+        exit(1)
+
+    CNT = args.length
+    if word1 is not None:
+        print(word1, end='')
+    for _ in range(CNT, 1, -1):
+        word2 = numpy.random.choice(list(statistics[word1].keys()), 1, list(statistics[word1].values()))[0]
+        if is_service_word(word2):
+            print('.', end='')
+            word1 = word2
+            continue
+        print(' ', word2, sep='', end='')
         word1 = word2
-        continue
-    print(' ', word2, sep='', end='')
-    word1 = word2
 
-last_word = numpy.random.choice(list(STATISTICS[word1].keys()), 1, list(STATISTICS[word1].values()))[0]
-if is_service_word(last_word):
-    print('.', end='')
-while is_service_word(last_word):
-    last_word = numpy.random.choice(list(STATISTICS[last_word].keys()), 1, list(STATISTICS[last_word].values()))
-print(' ', *last_word, sep='')
+    last_word = numpy.random.choice(list(statistics[word1].keys()), 1, list(statistics[word1].values()))[0]
+    if is_service_word(last_word):
+        print('.', end='')
+    while is_service_word(last_word):
+        last_word = numpy.random.choice(list(statistics[last_word].keys()), 1, list(statistics[last_word].values()))
+    print(' ', *last_word, sep='')
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -34,55 +34,72 @@ def parse_line(line, parse_all_collocations, parse_frequencies, lc):
         update_stat(parse_all_collocations, words, parse_frequencies)
 
 
-parser = argparse.ArgumentParser(description="Hi!", epilog="You're nice! Goodbye.")
-parser.add_argument('--input-dir', type=str, nargs=1,
-                    help='Way to directory, where your txt-files is, or stdin flow')
-parser.add_argument('--model', required=True, type=str, nargs=1, default='statistics.out',
-                    help="Way to file with it's name, where model will be written")
-parser.add_argument('--lc', action="store_true",
-                    help="If you want words in lowercase.")
-args = parser.parse_args()
+def reading(in_directory, all_colloc, freq, lc):
+    """
+    Read txt-files and count frequencies
+    :param in_directory: input directory
+    :param all_colloc: dictionary of collocations in text
+    :param freq: dictionary of frequencies
+    :param lc: if lower case is needed
+    :return: dict of collocations and dict of frequencies
+    """
+    if in_directory is not None:
+        in_directory = in_directory[0]
+        # Make a list of names of txt-files in input-dir
+        try:
+            files = os.listdir(in_directory)
+        except OSError:
+            print("Directory doesn't exist.")
+            exit(1)
+        txt_files = filter(lambda x: x.endswith('.txt'), files)
 
-BEGIN = END = None
-all_collocations = {}
-frequencies = {None: 1}
-lc = args.lc
+        # Read from each file
+        exist_txt_files = True
+        for filename in txt_files:
+            exist_txt_files = False
+            fin = open(filename, "r")
+            for new_line in fin:
+                parse_line(new_line, all_colloc, freq)
+        if exist_txt_files:
+            print("There're no txt-files in directory.")
+            exit(1)
+    else:
+        # Read from stdin flow
+        for new_line in sys.stdin.read():
+            parse_line(new_line, all_colloc, freq, lc)
 
-# Read txt-files and count frequencies
-input_directory = args.input_dir
-if input_directory is not None:
-    input_directory = input_directory[0]
-    # Make a list of names of txt-files in input-dir
-    try:
-        FILES = os.listdir(input_directory)
-    except OSError:
-        print("Directory doesn't exist.")
-        exit(1)
-    TXT_FILES = filter(lambda x: x.endswith('.txt'), FILES)
+    return all_colloc, freq
 
-    # Read from each file
-    exist_txt_files = True
-    for filename in TXT_FILES:
-        exist_txt_files = False
-        fin = open(filename, "r")
-        for new_line in fin.readlines():
-            parse_line(new_line, all_collocations, frequencies)
-    if exist_txt_files:
-        print("There're no txt-files in directory.")
-        exit(1)
-else:
-    # Read from stdin flow
-    lines = sys.stdin.read().split('\n')
-    for new_line in lines:
-        parse_line(new_line, all_collocations, frequencies, lc)
 
-# Normalize frequencies of collocations
-for word1, dict1 in all_collocations.items():
-    frequency = frequencies[word1]
-    for word2 in dict1.keys():
-        dict1[word2] /= frequency
+def main():
+    parser = argparse.ArgumentParser(description="Hi!", epilog="You're nice! Goodbye.")
+    parser.add_argument('--input-dir', type=str, nargs=1,
+                        help='Way to directory, where your txt-files is, or stdin flow')
+    parser.add_argument('--model', required=True, type=str, nargs=1, default='statistics.out',
+                        help="Way to file with it's name, where model will be written")
+    parser.add_argument('--lc', action="store_true",
+                        help="If you want words in lowercase.")
+    args = parser.parse_args()
 
-# Write dict of collocations in file
-fout = open(args.model[0], mode='wb')
-pickle.dump(all_collocations, fout)
-fout.close()
+    BEGIN = END = None
+    all_collocations = {}
+    frequencies = {None: 1}
+    lower_case = args.lc
+    input_directory = args.input_dir
+    all_collocations, frequencies = reading(input_directory, all_collocations,
+                                            frequencies, lower_case)
+
+    # Normalize frequencies of collocations
+    for word1, dict1 in all_collocations.items():
+        frequency = frequencies[word1]
+        for word2 in dict1.keys():
+            dict1[word2] /= frequency
+
+    # Write dict of collocations in file
+    fout = open(args.model[0], mode='wb')
+    pickle.dump(all_collocations, fout)
+    fout.close()
+
+
+if __name__ == '__main__':
+    main()
